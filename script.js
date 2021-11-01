7e195090d86ba33f0ba861072a6c8c83241f3775
Jbalto <91754085+Jbalto@users.noreply.github.com>
2021-11-01 11:58:08 -0600

    .

---
+++ script.js
@@ -0,0 +1,65 @@
+ // *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+  /*
+   Hello Yaakov
+   Good Bye John
+   Good Bye Jen
+   Good Bye Jason
+   Hello Paul
+   Hello Frank
+   Hello Larry
+   Hello Paula
+   Hello Laura
+   Good Bye Jim
+   WARNING!!! WARNING!!!
+   The code does NOT currently work! It is YOUR job to make it work
+   as described in the requirements and the steps in order to complete this
+   assignment.
+   WARNING!!! WARNING!!!
+   */
+
+// STEP 1:
+// Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
+
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
+
+// STEP 10:
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
+// See Lecture 50, part 1
+
+for (var i = 0; i < names.length; i++)
+// STEP 12:
+    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop. {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
