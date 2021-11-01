176cd718a63fbae36e6352d3e482fae3f59a9bb3
Jbalto <91754085+Jbalto@users.noreply.github.com>
2021-11-01 12:04:57 -0600

    .

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
- // *******************************
+(function () {
+  // *******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
 //
@@ -32,34 +33,45 @@
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="Jason";
-names[4]="Paul";
-names[5]="Frank";
-names[6]="Larry";
-names[7]="Paula";
-names[8]="Laura";
-names[9]="Jim";
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
+  for (var i = 0; i < names.length; i++) {
 
-for (var i = 0; i < names.length; i++)
-// STEP 12:
+    // STEP 11:
+    // Retrieve the first letter of the current name in the loop.
+    // Use the string object's 'charAt' function. Since we are looking for
+    // names that start with either upper case or lower case 'J'/'j', call
+    // string object's 'toLowerCase' method on the result so we can compare
+    // to lower case character 'j' afterwards.
+    // Look up these methods on Mozilla Developer Network web site if needed.
+     var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-    // name in the loop. {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-	}
-	else{
-		console.log("Hello "+ names[i])
-	}
-}
+    // name in the loop.
+    if (firstLetter === 'j') {
+       byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
